ca7b09b22395a0770658e09e0fd8c5c0bc91f3db
yeongseok mun <munyeongseok@gmail.com>
2023-12-23 16:55:02 +0900

    ProceduralTerrainGenerationDemo 빈 프로젝트 추가

---
+++ Source/Examples/WPF.SharpDX/ProceduralTerrainGenerationDemo/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿// The MIT License (MIT)
+// Copyright (c) 2018 Helix Toolkit contributors
+// See the LICENSE file in the project root for more information.
+
+using DemoCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProceduralTerrainGenerationDemo
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
