212cd1df4811e9b8b2a267b7f762d5e226fa4ff3
yeongseok mun <munyeongseok@gmail.com>
2024-01-03 23:04:06 +0900

    TerrainModel3D 추가

---
+++ Source/HelixToolkit.SharpDX.SharedModel/Element3D/TerrainModel3D.cs
@@ -0,0 +1,35 @@
+﻿// The MIT License (MIT)
+// Copyright (c) 2018 Helix Toolkit contributors
+// See the LICENSE file in the project root for more information.
+
+#if NETFX_CORE
+using Windows.UI.Xaml;
+
+namespace HelixToolkit.UWP
+#elif WINUI 
+using Microsoft.UI.Xaml;
+using HelixToolkit.SharpDX.Core;
+using HelixToolkit.SharpDX.Core.Model.Scene;
+namespace HelixToolkit.WinUI
+#else
+using System.Windows;
+#if COREWPF
+using HelixToolkit.SharpDX.Core;
+using HelixToolkit.SharpDX.Core.Model.Scene;
+#endif
+namespace HelixToolkit.Wpf.SharpDX
+#endif
+{
+    using Model;
+#if !COREWPF && !WINUI
+    using Model.Scene;
+#endif
+
+    public sealed class TerrainModel3D : Element3D
+    {
+        protected override SceneNode OnCreateSceneNode()
+        {
+            return new TerrainNode();
+        }
+    }
+}
